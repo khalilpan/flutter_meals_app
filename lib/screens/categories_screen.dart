bf0074f2874ae3ef084c3073849d025a586fa3b3
khalil <khalil.pan2@gmail.com>
2021-06-06 12:29:15 -0300

    adding 'meal item' class and passing parameters to it after clicking every category and adjusting images in meal item screen

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../data/dummy_data.dart';
+import '../data/dummy_categories.dart';
 import '../widgets/category_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/data/dummy_meals.dart';
+import 'package:meals_app/widgets/meal_item.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   const CategoryMealsScreen({key}) : super(key: key);
@@ -8,11 +10,26 @@ class CategoryMealsScreen extends StatelessWidget {
     final routeArgument =
         ModalRoute.of(context).settings.arguments as Map<String, String>;
     final categoryTitle = routeArgument['title'];
+    final categoryId = routeArgument['id'];
+    final categoryMeals = DUMMY_MEALS.where((item) {
+      return item.categories.contains(categoryId);
+    }).toList();
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text(categoryTitle),
-        ),
-        body: Text('The Recepies for The category!'));
+      appBar: AppBar(
+        title: Text(categoryTitle),
+      ),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return MealItem(
+              title: categoryMeals[index].title,
+              imageUrl: categoryMeals[index].imageUrl,
+              duration: categoryMeals[index].duration,
+              complexity: categoryMeals[index].complexity,
+              affordability: categoryMeals[index].affordability);
+        },
+        itemCount: categoryMeals.length,
+      ),
+    );
   }
 }
